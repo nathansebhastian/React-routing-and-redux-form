eccc9c163e90e0083946ac0305d645243d9e066a
Yonathan <yonathansebhastian@gmail.com>
2016-11-30 14:21:08 +0700

    form redux OK

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,39 @@
 import React, {Component} from 'react';
+import {reduxForm} from 'redux-form';
+import {createPost} from '../actions/index';
 
 class PostsNew extends Component {
   render () {
+    const { fields: {title, categories, content}, handleSubmit } = this.props;
+
     return (
-      <div>Create Form</div>
+      <form onSubmit={ handleSubmit(this.props.createPost) }>
+        <h3>Create A New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+        </div>
+
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories}/>
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea className="form-control" {...content} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     )}
 
 }
 
-export default PostsNew;
+// connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: form config, mapStateToProps, mapDispatchToProps
+
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew);
